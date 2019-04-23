270ec780e5e8de74ff001c5eea52877c724f2457
Caitlin Schrader <c.n.schrader22@gmail.com>
2019-04-22 19:31:07 -0500

    added switch-case statements

--- liri.js
+++ liri.js
@@ -1,3 +1,30 @@
+// initializing dotenv
 require("dotenv").config();
 
+// node's built in file system.
+const fs = require("fs");
+
+// grabbing our api keys
 const keys = require("./keys.js");
+
+const command = process.argv.shift(2);
+const question = process.argv.shift();
+
+
+// switch statement that will call functions depending on the user's input. 
+switch (command) {
+    case "find-on-spotify":
+    spotify();
+    break;
+
+    case "find-on-omdb":
+    movies();
+    break;
+
+    case "find-the-band":
+    bands();
+    break;
+
+    default:
+};
+
